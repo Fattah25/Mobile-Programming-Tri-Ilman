0eae4ceaecc81cc7a9e83c6143c4bba96573e567
Tri Ilman Almunawarah Fattah <fattahengler@gmail.com>
2022-10-20 11:45:49 +0700

    Menambahkan data

---
+++ 2-LinearLayout/app/src/main/java/com/example/latihan1/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.latihan1
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(saveInstanceState: Bundle?) {
+        super.onCreate(saveInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
